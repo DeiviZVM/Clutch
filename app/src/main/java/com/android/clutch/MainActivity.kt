c42e01ad66255817b3fa4e572456d001ddfdaafc
DavidVM <dvilches1409@gmail.com>
2023-08-03 11:06:13 +0200

    Proyecto iniciado

---
+++ app/src/main/java/com/android/clutch/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.android.clutch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.android.clutch.presentation.theme.ClutchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ClutchTheme (dynamicColor = false) {
+                // A surface container using the 'background' color from the theme
+                }
+            }
+        }
+    }
+}
